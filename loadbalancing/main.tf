fb02c145a1d892ae33634eb55b17ee01a41a6c46
Your Name <you@example.com>
2023-03-29 11:15:19 +0100

    lesson 94

--- loadbalancing/main.tf
+++ loadbalancing/main.tf
@@ -22,13 +22,13 @@ resource "aws_lb_target_group" "mtc_tg" {
 }
 
 resource "aws_lb_listener" "mtc_lb_listener" {
-    load_balancer_arn = 
-    port = var.listner_port
-    protocol = var.listener_protocol
-    default_action {
-     type = "forward"
-     target_group_arn =  
-    
-    }
-  
+  load_balancer_arn = aws_lb.mtc_lb.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mtc_tg.arn
+
+  }
+
 }
